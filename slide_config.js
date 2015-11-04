d1daaad9708e043be3b54b8daf05f4fd57af3063
Lucas Machado <git@lucasms.net>
2015-11-04 12:09:34 -0200

    Intro to HTML presentation

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'Introdução ao Front-end',
+    // subtitle: 'Ou seja, como funciona a ',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Lucas Machado',
+    // company: '',
+    // gplus: 'http://plus.google.com/1234567890',
+    twitter: '@luksm',
+    www: 'http://lucasms.net',
+    github: 'http://github.com/luksm'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
@@ -37,4 +37,3 @@ var SLIDE_CONFIG = {
     github: 'http://github.com/you'
   }*/]
 };
-
